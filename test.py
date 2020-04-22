46b75ecbcc90e93e0a04d61916d01a521025fe46
Neiso <djulian.ouatla@outlook.com>
2020-04-22 12:43:19 +0200

    fixing issues with git clone.

--- test.py
+++ test.py
@@ -38,14 +38,15 @@ class Testls(unittest.TestCase):
     def test_long_list_enabled(self):
         """
             Simple test with -l option enabled. 
-            Pay attention that if you modify any files, this test might
-            be wrong since the last modification date will change.
+            The test is commented because when you clone a git repo,
+            the last modifications date gets modified. You must change the
+            date to your local one in order to passe the test.
         """
         self.maxDiff = None
         output = subprocess.check_output("python3 ls.py -l", shell=True)
         expected = "drwxr-xr-x 8 djulian djulian 4096 2020-04-22 12:18 .git\ndrwxr-xr-x 2 djulian djulian 4096 2020-04-22 12:16 __pycache__\n-rw-r--r-- 1 djulian djulian 4135 2020-04-22 12:15 function.py\n-rwxr-xr-x 1 djulian djulian 2045 2020-04-22 12:14 ls.py\n-rw-r--r-- 1 djulian djulian  512 2020-04-21 12:33 README.md\n-rwxr-xr-x 1 djulian djulian 5703 2020-04-22 12:20 test.py\ndrwxr-xr-x 4 djulian djulian 4096 2020-04-22 10:49 tests\n"
         output2 = subprocess.check_output("python3 ls.py -l ./tests", shell=True)
-        expected2 = "drwxr-xr-x 3 djulian djulian 4096 2020-04-22 12:28 path\ndrwxr-xr-x 2 djulian djulian 4096 2020-04-22 10:49 test3\n"
+        expected2 ="drwxr-xr-x 3 djulian djulian 4096 2020-04-22 12:28 path\ndrwxr-xr-x 2 djulian djulian 4096 2020-04-22 10:49 test3\n"
         output3 = subprocess.check_output("python3 ls.py -l ./tests/path/to/folder/", shell=True)
         expected3 = "-rwxr-xr-x 1 djulian djulian 0 2020-04-22 10:48 another_file\n-rwxr--r-- 1 djulian djulian 0 2020-04-22 10:48 some_file\n"        
         output4 = subprocess.check_output("python3 ls.py -l ./tests/path/t", shell=True)
@@ -53,10 +54,10 @@ class Testls(unittest.TestCase):
         output5 = subprocess.check_output("python3 ls.py -l ./tests/path/to", shell=True)
         expected5 = "-rw-r--r-- 1 djulian djulian    0 2020-04-22 10:49 acces.log\ndrwxr-xr-x 2 djulian djulian 4096 2020-04-22 10:48 another_dir\n-rw-r--r-- 1 djulian djulian    0 2020-04-22 10:49 error.log\ndrwxr-xr-x 2 djulian djulian 4096 2020-04-22 10:57 folder\n-rw-r--r-- 1 djulian djulian    0 2020-04-22 10:48 Hello.txt\ndrwxr-xr-x 2 djulian djulian 4096 2020-04-22 10:57 some_dir\n-rw-r--r-- 1 djulian djulian    0 2020-04-22 10:48 World.txt\n"
         # self.assertEqual(output.decode("utf-8"), expected)
-        self.assertEqual(output2.decode("utf-8"), expected2)
-        self.assertEqual(output3.decode("utf-8"), expected3)
-        self.assertEqual(output4.decode("utf-8"), expected4)
-        self.assertEqual(output5.decode("utf-8"), expected5)
+        # self.assertEqual(output2.decode("utf-8"), expected2)
+        # self.assertEqual(output3.decode("utf-8"), expected3)
+        # self.assertEqual(output4.decode("utf-8"), expected4)
+        # self.assertEqual(output5.decode("utf-8"), expected5)
 
     def test_error(self):
         """The program shouldn't exit unexpectedly."""
